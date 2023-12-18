06e1c0371a767342995698e010579463def62656
TomDeeterink1 <deeterinktom@gmail.com>
2023-12-18 17:50:15 +0100

    first 3 hovers

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,28 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+// bibberen
+let bibberen = document.querySelector('.shake')
+
+bibberen.addEventListener('mouseover', bibbertje)
+
+function bibbertje(){
+  bibberen.classList.toggle('bibber')
+}
+
+// draaien
+let draaien = document.querySelector('.turn')
+
+draaien.addEventListener('mouseover', draaihem)
+
+function draaihem(){
+  draaien.classList.toggle('spin')
+}
+// floataround
+let float = document.querySelector('.floataround')
+
+float.addEventListener('mouseover', floatit)
+
+function floatit(){
+  float.classList.toggle('float')
+}
